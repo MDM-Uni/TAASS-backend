a67db2744e583d2c24b297a54ec55ecb8401be34
Marco <marcoscale98libero.it>
2021-12-21 14:45:15 +0100

    initial commit

---
+++ src/main/java/com/example/taassbackend/TaassBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.taassbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaassBackendApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(TaassBackendApplication.class, args);
+   }
+
+}
---
+++ src/test/java/com/example/taassbackend/TaassBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taassbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaassBackendApplicationTests {
+
+   @Test
+   void contextLoads() {
+   }
+
+}
