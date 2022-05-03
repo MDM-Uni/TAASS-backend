8b96ef740eb76f9a7d17ac6a36fc3b545eab7bd7
marcoscale98 <marcoscale98@libero.it>
2022-05-03 12:17:14 +0200

    aggiunti bash per loggare i microservizi

--- bash/delete_all_deploy.sh
+++ bash/delete_all_deploy.sh
@@ -2,20 +2,48 @@ kubectl delete -n default deployment pgadmin
 
 kubectl delete -n default deployment rabbitmq
 
-kubectl delete -n default deployment dbpostgresql-negozio
+kubectl delete -n default deployment dbpostgresqlnegozio
 
-kubectl delete -n default deployment dbpostgresql-utente
+kubectl delete -n default deployment dbpostgresqlutente
 
-kubectl delete -n default deployment dbpostgresql-ospedale
+kubectl delete -n default deployment dbpostgresqlospedale
 
-kubectl delete -n default deployment app-ospedale
+kubectl delete -n default deployment appospedale
 
-kubectl delete -n default deployment app-utente
+kubectl delete -n default deployment apputente
 
-kubectl delete -n default deployment app-negozio
+kubectl delete -n default deployment appnegozio
 
-kubectl delete -n default deployment frontend-web
+kubectl delete -n default deployment frontendweb
 
+kubectl delete -n default service pgadmin
 
+kubectl delete -n default service rabbitmq
 
+kubectl delete -n default service dbpostgresqlnegozio
 
+kubectl delete -n default service dbpostgresqlutente
+
+kubectl delete -n default service dbpostgresqlospedale
+
+kubectl delete -n default service appospedale
+
+kubectl delete -n default service apputente
+
+kubectl delete -n default service appnegozio
+
+kubectl delete -n default service frontendweb
+
+kubectl delete -n default service app-ospedale
+
+kubectl delete -n default service app-utente
+
+kubectl delete -n default service app-utente
+
+kubectl delete -n default service frontend-web
+
+kubectl delete -n default service dbpostgresql-negozio
+
+kubectl delete -n default service dbpostgresql-utente
+
+kubectl delete -n default service dbpostgresql-ospedale
