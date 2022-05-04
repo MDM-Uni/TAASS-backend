d771c5dcd88f6d1a7e034fe36b46d7a214c843b8
marcoscale98 <marcoscale98@libero.it>
2022-05-04 10:36:05 +0200

    sembra funzionare ma ho un problema con l'aggiunta di un animale

--- bash/apply_all.sh
+++ bash/apply_all.sh
@@ -1,6 +1,1 @@
-kubectl apply $(ls pgadmin*.yaml | awk ' { print " -f " $1 } ')
-kubectl apply $(ls rabbitmq*.yaml | awk ' { print " -f " $1 } ')
-kubectl apply $(ls dbpostgresql*.yaml | awk ' { print " -f " $1 } ')
-sleep 25
-kubectl apply $(ls app*.yaml | awk ' { print " -f " $1 } ')
-kubectl apply $(ls frontend*.yaml | awk ' { print " -f " $1 } ')
+kubectl apply -f kube
